d6ae763dc42ea8780896a0024c13baf1224a2ce3
Rizvi <s.rizvi@dxc.com>
2023-09-01 11:45:23 +0530

    config-server-added

---
+++ ConfigServer/src/main/java/com/hr/ConfigServer/ConfigServerApplication.java
@@ -0,0 +1,17 @@
+package com.hr.ConfigServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+
+		System.out.println("Configuration Server is working");
+	}
+
+}
